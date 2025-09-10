78c4258fc2c60a5af9f1a57d430634aa2740b17c
Andreas Hrubak <ad132454@uucp.hu>
2025-09-10 12:37:19 +0200

    show attachments and announcement creator

--- dashboard.py
+++ dashboard.py
@@ -4,21 +4,43 @@ import json
 
 from utils import color, gclassroom, logger
 
-def indent(t, i='\t'):
+def indent(t, level=1, i='\t'):
 	import re
-	return re.sub(r'^', i, t, 0, re.MULTILINE)
+	return re.sub(r'^', i*level, t, 0, re.MULTILINE)
 
+def firstOrNone(iterable):
+	try:
+		first = iterable[0]
+	except IndexError:
+		first = None
+	return first
 
 Classroom = gclassroom.Classroom()
 Classroom.initialize()
 
-curses = Classroom.service.courses().list(pageSize=100).execute().get('courses')
+curses_object = Classroom.service.courses()
+curses = curses_object.list(pageSize=100).execute().get('courses')
 
 for curse in curses:
-	print("{name}".format_map(curse))
-	announcements = Classroom.service.courses().announcements().list(courseId=curse['id']).execute().get('announcements')
-	for announ in announcements:
-		datetime_info = '\t'.join([announ['creationTime'], announ['updateTime'] if announ['updateTime'] != announ['creationTime'] else ''])
-		print(indent(datetime_info))
+	announcements = curses_object.announcements().list(courseId=curse['id']).execute().get('announcements')
+	teachers = curses_object.teachers().list(courseId=curse['id']).execute().get('teachers')
+	
+	for announ in reversed(announcements):
+		creator_teacher = firstOrNone([teacher['profile']['name']['fullName'] for teacher in teachers if teacher['userId'] == announ['creatorUserId']])
+		if creator_teacher is not None:
+			print(f"From: {creator_teacher}")
+		print(f"Curse: {curse['name']}")
+		print(f"Date-Created: {announ['creationTime']}")
+		if announ['updateTime'] != announ['creationTime']:
+			print(f"Date-Updated: {announ['updateTime']}")
+		
+		for material in announ.get('materials', []):
+			if 'driveFile' in material and 'driveFile' in material['driveFile']:
+				driveFile = material['driveFile']['driveFile']
+				print(f"Attachment: {driveFile['title']} <{driveFile['alternateLink']}>")
+			else:
+				print(f"Attachment-JSON: {json.dumps(material)}")
+		print()
 		text = announ['text']
-		print(indent(indent(text)))
+		print(indent(text))
+		print()
